e78af25e49411e2caed97d5b6ea08453f2528aed
Haryadi Widianto <Haryadi Widianto@DESKTOP-22OELT1>
2020-02-09 13:38:44 +0700

    Methods

--- Program.cs
+++ Program.cs
@@ -1,17 +1,48 @@
 ﻿using System;
-using System.Linq;
-using System.Activities;
-using System.Activities.Statements;
+using System.Collections.Generic;
+using Numbers;
 
 namespace Methods
 {
 
     class Program
     {
+        public void test(int ju, out int y, out string x)
+        {
+            y = ( 50 + ju );
+            x = "Keren";
+
+        }
+
         static void Main(string[] args)
         {
-            Activity workflow1 = new Workflow1();
-            WorkflowInvoker.Invoke(workflow1);
+            /*
+            Program p = new Program();
+            p.test(20, out int yt, out string xt);
+            Console.WriteLine("Hello, world! {0}, {1}", yt, xt);
+            */
+
+            int[] i = { 5, 8, 9, 12, 23, 6, 12, 78, 2, 442, 344, 67 };
+            int l = i.Length;
+
+            Console.WriteLine(@"Nilai terbesar : " + FindNumber.FindMax(i));
+
+           
+            FindNumber.FindMinMax(i, out int min, out int max);
+
+            Console.WriteLine($"Nilai Terkecil : {min}");
+            Console.WriteLine($"Nilai Terbesar : {max}");
+
+
+            Console.WriteLine($"Nilai Terkecil : " + FindNumber.FindMin(i, l));
+
+            int c = 10;
+            FindNumber.addNumber(ref c, 20);
+            Console.WriteLine($"Nilai Setelah Ditambah : {c}");
+
+            Console.ReadKey();
+
         }
+
     }
 }
