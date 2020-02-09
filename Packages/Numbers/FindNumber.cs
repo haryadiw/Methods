fc8dbfd027b7e030698883a0173734969ee66e62
Haryadi Widianto <Haryadi Widianto@DESKTOP-22OELT1>
2020-02-09 13:41:09 +0700

    Add

---
+++ Packages/Numbers/FindNumber.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Numbers
+{
+    public class FindNumber
+    {
+
+        static public int FindMax(int[] @params)
+        {
+
+            int result = 0;
+
+            foreach(int i in @params)
+            {
+                if(i > result)
+                {
+                    result = i;
+                }
+            }
+
+            return result;
+
+        }
+
+        static public void FindMinMax(int[] @params, out int min, out int max)
+        {
+
+            min = 0;
+            max = 0;
+
+            foreach (int i in @params)
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+
+                if (i < min)
+                {
+                    min = i;
+                }
+
+            }
+
+
+        }
+
+        static public int FindMin(int[] @params, int n)
+        {
+            
+            if (n == 1)
+                return @params[0];
+
+            return Math.Min(@params[n - 1], FindMin(@params, n - 1));
+
+
+        }
+
+        static public void addNumber(ref int a, int b)
+        {
+            a += b;
+        }
+
+
+    }
+}
